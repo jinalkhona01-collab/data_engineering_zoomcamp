6e83b004b0db790d33dfb9db24a6ad304be01345
Jinal Khona <jinal.khona01@gmail.com>
2026-03-14 00:24:07 +0100

    Add files via upload

---
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,27 @@
+with source as (
+    select * 
+    from {{ source('raw', 'fhv_tripdata_2019') }}
+),
+
+renamed as (
+    select
+        cast(dispatching_base_num as string) as dispatching_base_num,
+    
+        cast(pickup_datetime as timestamp) as pickup_datetime,  
+        cast(dropoff_datetime as timestamp) as dropoff_datetime,
+
+        cast(pulocationid as integer) as pickup_location_id,
+        cast(dolocationid as integer) as dropoff_location_id,
+
+        cast(sr_flag as integer) as shared_ride_flag,
+        cast(affiliated_base_number as string) as affiliated_base_number,
+
+    from source
+    where dispatching_base_num is not null
+)
+
+select * from renamed
+
+{% if target.name == 'dev' %}
+where pickup_datetime >= '2019-01-01' and pickup_datetime < '2019-02-01'
+{% endif %}
